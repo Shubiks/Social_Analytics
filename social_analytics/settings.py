0dec780c8977c5cfe9da56b5337c2d4d83e55bce
Shubiksha <shubiksha.b@finestcoder.com>
2025-03-26 00:24:04 +0530

    fetch basic data

--- social_analytics/settings.py
+++ social_analytics/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'youtube_analytics',
+    'instagram_analytics',
 ]
 
 MIDDLEWARE = [
